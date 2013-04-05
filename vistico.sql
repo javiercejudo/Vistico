a0b0dc48a95cf8bbb7ead49aa0e631ad5248334e
Javier Cejudo <javier@javiercejudo.com>
2013-04-06 00:03:43 +1200

    cleans up schema

--- vistico.sql
+++ vistico.sql
@@ -1,21 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 3.4.5deb1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Dec 27, 2011 at 12:35 AM
--- Server version: 5.1.58
--- PHP Version: 5.3.6-13ubuntu3.3
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
 --
 -- Database: `vistico`
 --
@@ -50,7 +32,3 @@ CREATE TABLE IF NOT EXISTS `Project` (
   PRIMARY KEY (`project_id`),
   UNIQUE KEY `short_name` (`short_name`,`full_name`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
